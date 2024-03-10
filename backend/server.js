af32135cc5a2b14a58e3d799a3b9362a0bc67b57
Swa-aham <sohammandaviya@gmail.com>
2024-03-10 22:29:07 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 require('dotenv').config();
 
-const express = require('express');
-const workoutRoutes = require('./routes/workout')
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express();
 
+// routes
 app.use('/api/workouts', workoutRoutes)
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to database & listening on port', process.env.PORT);
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 // routes
 // app.get('/', (req, res) => {
@@ -18,8 +32,3 @@ app.use('/api/workouts', workoutRoutes)
 //     res.write('I Love you, Soham');
 //     res.end();
 // })
-
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT);
-})
